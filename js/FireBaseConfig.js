b55ae06725a29609cc95611aca9662f0c716fd1d
Ben Dreyer <38509839+bdreyerr@users.noreply.github.com>
2019-07-29 11:58:34 -0600

    js speration
    seperated javascript into seperate folder

---
+++ js/FireBaseConfig.js
@@ -0,0 +1,13 @@
+      // Your web app's Firebase configuration
+      var firebaseConfig = {
+        apiKey: "AIzaSyAVo95Uhru5y8w29KM5MA3VVhLNNvbpxZU",
+        authDomain: "sublease-5a504.firebaseapp.com",
+        databaseURL: "https://sublease-5a504.firebaseio.com",
+        projectId: "sublease-5a504",
+        storageBucket: "sublease-5a504.appspot.com",
+        messagingSenderId: "877733738268",
+        appId: "1:877733738268:web:ae879c163293a99a"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      var database = firebase.database();
