a611f8a9443559c5620ae1a21fa578d4d1943842
pcmason <35756257+pcmason@users.noreply.github.com>
2019-12-13 16:56:54 -0500

    Update App.js

--- js/App.js
+++ js/App.js
@@ -1,23 +1,28 @@
 import React from "react"
 import {BrowserRouter,Switch,Route} from "react-router-dom"
-import Header from './Components/Header'
 import NavBar from './Components/NavBar'
 import Home from './Components/Home'
 import About from './Components/About'
 import Messages from './Components/Messages'
 import Signup from './Components/Signup'
+import Listing from './Components/Listing'
+import Room from './Components/Room'
+import Favorites from './Components/Favorites'
 
 const App = () =>{
     return(
         <div>
-            <Header />
-            <NavBar />
+            
             <BrowserRouter>
+                <NavBar />
                 <Switch>
                     <Route path="/" component={Home} exact/>
-                    <Route path="/about" component={About}/>
-                    <Route path="/messages" component={Messages}/>
-                    <Route path="/signup" component={Signup}/>
+                    <Route path="/About" component={About}/>
+                    <Route path="/Favorites" component={Favorites}/>
+                    <Route path="/Room" component={Room}/>
+                    <Route path="/Listing" component={Listing}/> 
+                    <Route path="/Messages" component={Messages}/>
+                    <Route path="/Signup" component={Signup}/>
                 </Switch>
             </BrowserRouter>
         </div>
