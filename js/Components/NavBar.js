726f7c1baddc81f30f2b0193a9991c791ae2404b
pcmason <35756257+pcmason@users.noreply.github.com>
2019-12-13 16:56:23 -0500

    Update NavBar.js

--- js/Components/NavBar.js
+++ js/Components/NavBar.js
@@ -1,19 +1,26 @@
 import React from 'react'
-import {NavLink} from 'react-router-dom'
+import {BrowserRouter, NavLink} from 'react-router-dom'
 import './NavBar.css'
+import Logo from './img/logo.png'
 
 const NavBar = () =>{
     return(
-        <div className = "menu">
-            <div className="table">
-                <ul className="hor_lst">
-                    <li><NavLink to="/"exact>Home</NavLink></li>
-                    <li><NavLink to="/about">About</NavLink></li>
-                    <li><NavLink to="/messages">Messages</NavLink></li>
-                    <li><NavLink to="/signup">Signup</NavLink></li>
-                </ul>
+        // <BrowserRouter>
+            <div className = "menu">
+                <div className="table">
+                    <ul className="hor_lst">
+                        <li><NavLink to="/"exact>Home</NavLink></li>
+                        <li><NavLink to="/Listing">Create/Manage Listings</NavLink></li>
+                        <li><NavLink to ="/Room">Find a Room</NavLink></li>
+                        <li><img src={Logo}width="150"height="150"></img></li>
+                        <li><NavLink to="/Favorites">Favorited Listings</NavLink></li>
+                        <li><NavLink to="/Messages">Messages</NavLink></li>
+                        <li><NavLink to="/About">About</NavLink></li>
+                        <li><NavLink to="/Signup">Signup/Signin</NavLink></li>
+                    </ul>
+                </div>
             </div>
-        </div>
+        // </BrowserRouter>
     )
 }
 
