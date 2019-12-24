074120193bde50a61731fdfe9b2a609ed79f0eb2
Mitch Hansen <mitchames@live.com>
2019-12-24 14:01:55 -0800

    Added Google Maps API

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import NavBar from './Components/NavBar.js';
-import Map from './Components/Map.js';
+import MapContainer from './Components/MapContainer.js';
 import Footer from './Components/Footer.js';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <Map/>
+      <MapContainer/>
       <Footer/>
     </div>
   );
---
+++ client/src/Components/MapContainer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+export class MapContainer extends React.Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+         lat: 37.349829,
+         lng: -121.938955
+        }}
+      />
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyDDUtqrSwrxpOBCtFaupAhwjParxJ3LSpo",
+  authDomain: "leasr-scu.firebaseapp.com",
+  databaseURL: "https://leasr-scu.firebaseio.com",
+  projectId: "leasr-scu",
+  storageBucket: "",
+  messagingSenderId: "630499067439",
+  appId: "1:630499067439:web:90e7c534a6bf68bd"
+})(MapContainer);
