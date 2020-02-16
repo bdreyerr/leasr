202ddffdf15e314b73b2516028217d6a58a4ff32
mitchahan <mitchames@live.com>
2020-02-16 15:34:00 -0800

    Login and Signup styling

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { Auth } from "aws-amplify";
 import { useFormFields } from "../libs/hooksLib";
 
@@ -26,29 +27,38 @@ export default function Login(props) {
   }
 
   return (
-    <div className="Login">
-      <form onSubmit={handleSubmit}>
-        <FormGroup controlId="email" bsSize="large">
-          <FormLabel>Email</FormLabel>
-          <FormControl
-            autoFocus
-            type="email"
-            value={fields.email}
-            onChange={handleFieldChange}
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
-          <FormLabel>Password</FormLabel>
-          <FormControl
-            value={fields.password}
-            onChange={handleFieldChange}
-            type="password"
-          />
-        </FormGroup>
-        <Button block bsSize="large" disabled={!validateForm()} type="submit">
-          Login
-        </Button>
-      </form>
+    <div className="mt-5 mb-5 bg-light rounded mx-auto w-25 auth-wrapper">
+      <div className="p-5 auth-inner">
+        <form onSubmit={handleSubmit}>
+          <h3>Sign In</h3>
+          <FormGroup controlId="email" bsSize="large">
+            <FormLabel>Email</FormLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={fields.email}
+              onChange={handleFieldChange}
+              className="p-2"
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <FormLabel>Password</FormLabel>
+            <FormControl
+              value={fields.password}
+              onChange={handleFieldChange}
+              type="password"
+              className="p-2"
+            />
+          </FormGroup>
+          <Button block bsSize="large" className="p-2 pb-0 btn btn-success btn-block" disabled={!validateForm()} type="submit">
+            Login
+          </Button>
+
+          <p className="forgot-password text-right text-success">
+              <Link className="text-success" href="#">Forgot password?</Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 }
--- client/src/Components/Signup.js
+++ client/src/Components/Signup.js
@@ -5,6 +5,7 @@ import {
   FormControl,
   FormLabel
 } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { useFormFields } from "../libs/hooksLib";
 import { Auth } from "aws-amplify";
 
@@ -84,41 +85,55 @@ export default function Signup(props) {
 
   function renderForm() {
     return (
-      <form onSubmit={handleSubmit}>
-        <FormGroup controlId="email" bsSize="large">
-          <FormLabel>Email</FormLabel>
-          <FormControl
-            autoFocus
-            type="email"
-            value={fields.email}
-            onChange={handleFieldChange}
-          />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large">
-          <FormLabel>Password</FormLabel>
-          <FormControl
-            type="password"
-            value={fields.password}
-            onChange={handleFieldChange}
-          />
-        </FormGroup>
-        <FormGroup controlId="confirmPassword" bsSize="large">
-          <FormLabel>Confirm Password</FormLabel>
-          <FormControl
-            type="password"
-            onChange={handleFieldChange}
-            value={fields.confirmPassword}
-          />
-        </FormGroup>
-        <Button
-          block
-          type="submit"
-          bsSize="large"
-          disabled={!validateForm()}
-        >
-          Signup
-        </Button>
-      </form>
+      <div className="mt-5 mb-5 bg-light rounded mx-auto w-25 auth-wrapper">
+        <div className="p-5 auth-inner">
+          <form onSubmit={handleSubmit}>
+            <h3>Sign Up</h3>
+          
+            <FormGroup controlId="email" bsSize="large">
+              <FormLabel>Email</FormLabel>
+              <FormControl
+                autoFocus
+                type="email"
+                value={fields.email}
+                onChange={handleFieldChange}
+                className="p-2"
+              />
+            </FormGroup>
+            <FormGroup controlId="password" bsSize="large">
+              <FormLabel>Password</FormLabel>
+              <FormControl
+                type="password"
+                value={fields.password}
+                onChange={handleFieldChange}
+                className="p-2"
+              />
+            </FormGroup>
+            <FormGroup controlId="confirmPassword" bsSize="large">
+              <FormLabel>Confirm Password</FormLabel>
+              <FormControl
+                type="password"
+                onChange={handleFieldChange}
+                value={fields.confirmPassword}
+                className="p-2"
+              />
+            </FormGroup>
+            <Button
+              block
+              type="submit"
+              bsSize="large"
+              disabled={!validateForm()}
+              className="p-2 pb-0 btn btn-success btn-block"
+            >
+              Signup
+            </Button>
+
+            <p className="forgot-password text-success text-right">
+                <Link className="text-success" to="/login">Already registered? Sign in</Link>
+            </p>
+          </form>
+        </div>
+      </div>
     );
   }
 
